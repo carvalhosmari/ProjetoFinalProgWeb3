65de7eb4325e0e03ca6fe1fcf291b728d713c432
carvalhosmari <mscarvall@gmail.com>
2022-09-15 16:09:34 -0300

    Inclusão de data annotations

--- ProgWeb3APIEventos.Core/Model/CityEvent.cs
+++ ProgWeb3APIEventos.Core/Model/CityEvent.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProgWeb3APIEventos.Core.Model
 {
     public class CityEvent
     {
         public long IdEvent { get; set; }
+        [Required(ErrorMessage = "O evento precisa ter um título.")]
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "A data não pode ser vazia.")]
         public DateTime DateHourEvent { get; set; }
+        [Required(ErrorMessage = "É preciso informar o local do evento.")]
         public string Local { get; set; }
         public string Address { get; set; }
         public decimal Price { get; set; }
--- ProgWeb3APIEventos.Core/Model/EventReservation.cs
+++ ProgWeb3APIEventos.Core/Model/EventReservation.cs
@@ -1,10 +1,15 @@
-﻿namespace ProgWeb3APIEventos.Core.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgWeb3APIEventos.Core.Model
 {
     public class EventReservation
     {
         public long IdReservation { get; set; }
+        [Required(ErrorMessage = "É preciso informar o Id do evento que se deseja fazer a reserva.")]
         public long IdEvent { get; set; }
+        [Required(ErrorMessage = "A reserva deve possuir um titular.")]
         public string PersonName { get; set; }
+        [Required(ErrorMessage = "É preciso informar no mínimo uma pessoa para a reserva.")]
         public long Quantity { get; set; }
     }
 }
